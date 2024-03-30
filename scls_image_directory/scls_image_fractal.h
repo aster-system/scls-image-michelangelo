1dd92787df84a837b117c9abb7680b37fc759152
unknown <mmattmmo@gmail.com>
2024-03-30 21:47:53 +0100

    Add burning ship fractal

--- scls_image_directory/scls_image_fractal.h
+++ scls_image_directory/scls_image_fractal.h
@@ -20,6 +20,54 @@
 namespace scls
 {
     // Return an image with a Mandelbrot fractal drawn on it
+    inline Image* burning_ship_fractal(unsigned short width, unsigned short height) {
+        // Define the size of the fractale
+        double min_x = -2.5;
+        double min_y = -2.5;
+        double width_fractal = 5;
+        double height_fractal = 5;
+
+        // Create the image
+        Image* img = new Image(width, height, 255, 255, 255);
+
+        // Draw the fractal pixel by pixel
+        for(int i = 0;i<width;i++) {
+            for(int j = 0;j<height;j++) {
+                // Define each needed complex
+                double c_imaginary = min_y + (j / static_cast<double>(height)) * height_fractal;
+                double c_real = min_x + (i / static_cast<double>(width)) * width_fractal;
+                double z_imaginary = 0;
+                double z_real = 0;
+
+                // Define the needed variables to test the function
+                unsigned char it = 0;
+                unsigned char max_iteration = 20;
+                unsigned char max_size = 100;
+
+                // Do the function
+                while(it < max_iteration && sqrt(z_imaginary * z_imaginary + z_real * z_real) < max_size) {
+                    double temp_real = z_real;
+                    z_real = abs(temp_real * temp_real - z_imaginary * z_imaginary - 1 + c_real);
+                    z_imaginary = abs(2 * z_imaginary * temp_real + c_imaginary);
+
+                    it++;
+                }
+
+                // Draw the pixel
+                if(it == max_iteration) {
+                    img->set_pixel(i, j, 0, 0, 0);
+                }
+                else {
+                    img->set_pixel(i, j, (static_cast<double>(it) / static_cast<double>(max_iteration)) * 255.0, (static_cast<double>(it) / static_cast<double>(max_iteration)) * 255.0, 255 - (static_cast<double>(it) / static_cast<double>(max_iteration)) * 255.0);
+                }
+            }
+        }
+
+        // Return the image
+        return img;
+    };
+
+    // Return an image with a Mandelbrot fractal drawn on it
     inline Image* mandelbrot_fractal(unsigned short width, unsigned short height) {
         // Define the size of the fractale
         double min_x = -2.5;
