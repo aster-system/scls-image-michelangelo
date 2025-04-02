0a4ad05b7f730bbc1aaa5756fefd9de53662f7f3
Matt Mmo <mmattmmo@gmail.com>
2025-04-02 22:37:21 +0200

    Update text style system

--- sources/scls_image_core.cpp
+++ sources/scls_image_core.cpp
@@ -55,16 +55,23 @@ namespace scls {
         // Get the color
         Color to_return(255, 255, 255);
         std::vector<std::string> cutted = cut_string(source, ",");
-        for(int i = 0;i<static_cast<int>(cutted.size());i++) {
-            while(cutted[i][0] == ' ') cutted[i] = cutted[i].substr(1, cutted[i].size() - 1);
-            while(cutted[i][cutted[i].size() - 1] == ' ') cutted[i] = cutted[i].substr(0, cutted[i].size() - 1);
+        if(cutted.size() > 1) {
+            for(int i = 0;i<static_cast<int>(cutted.size());i++) {
+                while(cutted[i][0] == ' ') cutted[i] = cutted[i].substr(1, cutted[i].size() - 1);
+                while(cutted[i][cutted[i].size() - 1] == ' ') cutted[i] = cutted[i].substr(0, cutted[i].size() - 1);
 
-            // Apply the color
-            if(i == 0) to_return.set_red(std::stoi(cutted[i]));
-            else if(i == 1) to_return.set_green(std::stoi(cutted[i]));
-            else if(i == 2) to_return.set_blue(std::stoi(cutted[i]));
-            else if(i == 3) to_return.set_alpha(std::stoi(cutted[i]));
+                // Apply the color
+                if(i == 0) to_return.set_red(std::stoi(cutted[i]));
+                else if(i == 1) to_return.set_green(std::stoi(cutted[i]));
+                else if(i == 2) to_return.set_blue(std::stoi(cutted[i]));
+                else if(i == 3) to_return.set_alpha(std::stoi(cutted[i]));
+            }
         }
+        else if (cutted.size() == 1) {
+            // Precise color
+            if(cutted[0] == std::string("red")){to_return = Color(255, 0, 0);}
+        }
+
         return to_return;
     };
 
@@ -76,10 +83,20 @@ namespace scls {
             std::string current_attribute_name = current_attribute.name;
             std::string current_attribute_value = current_attribute.value;
 
-            if(current_attribute_name == "red") {color.set_red(std::stoi(current_attribute_value));}
-            else if(current_attribute_name == "green") {color.set_green(std::stoi(current_attribute_value));}
-            else if(current_attribute_name == "blue") {color.set_blue(std::stoi(current_attribute_value));}
-            else if(current_attribute_name == "alpha") {color.set_alpha(std::stoi(current_attribute_value)); }
+            if(current_attribute_value == std::string()) {
+                // Defined color
+                if(current_attribute_name == "red") {color = Color(255, 0, 0);}
+                else if(current_attribute_name == "green") {color = Color(0, 255, 0);}
+                else if(current_attribute_name == "blue") {color = Color(0, 0, 255);}
+                else if(current_attribute_name == "white") {color = Color(255, 255, 255);}
+            }
+            else {
+                // Precise datas for the color
+                if(current_attribute_name == "red") {color.set_red(std::stoi(current_attribute_value));}
+                else if(current_attribute_name == "green") {color.set_green(std::stoi(current_attribute_value));}
+                else if(current_attribute_name == "blue") {color.set_blue(std::stoi(current_attribute_value));}
+                else if(current_attribute_name == "alpha") {color.set_alpha(std::stoi(current_attribute_value));}
+            }
         }
         return color;
     }
