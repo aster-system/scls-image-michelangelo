61674d3e16eac8b22b0301acc0932b9c697e2865
Matt <mmattmmo@gmail.com>
2026-01-15 07:30:18 +0100

    Update to the new SCLS version

--- test.cpp
+++ test.cpp
@@ -23,34 +23,10 @@ SCLS_INIT;
 using namespace scls;
 
 int main() {
-	std::shared_ptr<Boolean> b = std::make_shared<Boolean>("a");b.get()->add_and("b");
-	std::shared_ptr<Boolean> c = std::make_shared<Boolean>("c");c.get()->add_and("d");
-	std::shared_ptr<Boolean> d = b.get()->clone();d.get()->add_or(c.get());
-	c = std::make_shared<Boolean>("b");c.get()->add_or("c");c.get()->add_or("d");
-	d.get()->add_and(c.get());
-	std::cout << "O " << d.get()->to_std_string(0) << std::endl;
-
-	std::string diff = std::string("---------------------------------");
-	std::string to_return = std::string("a | b | c | d | ((a . b) + (c . d)) . (b + c + d)\n") + diff + std::string("\n");
-
-	for(int i = 0;i<2;i++) {
-        for(int j = 0;j<2;j++) {
-            for(int k = 0;k<2;k++) {
-                for(int l = 0;l<2;l++) {
-                    Unknowns_Container a = Unknowns_Container();
-                    a.create_unknown<Boolean_Unknown>("a")->value = (i == 1);
-                    a.create_unknown<Boolean_Unknown>("b")->value = (j == 1);
-                    a.create_unknown<Boolean_Unknown>("c")->value = (k == 1);
-                    a.create_unknown<Boolean_Unknown>("d")->value = (l == 1);
-
-                    to_return += std::to_string(i) + std::string(" | ") + std::to_string(j) + std::string(" | ") + std::to_string(k) + std::string(" | ") + std::to_string(l) + std::string(" |         ") + d.get()->replace_unknowns(&a).get()->to_std_string(0) + std::string("\n") + diff + std::string("\n");
-                }
-            }
-        }
-	}
-
+	std::shared_ptr<Boolean> d = string_to_algebra_element<Boolean>("(((a . b . f) + (c . d . e)) . (b + c + d) . ((a . d) + (b . c) + e)) + ((e . f) + (b . e) + (b . f))");
+	std::string to_return = d.get()->verity_table();
 	write_in_file("tests/boole.txt", to_return);
-	print("SCLS", to_return);
+	std::cout << to_return << std::endl;
 
     return 0;
 }
